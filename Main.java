57208434ce281953ab0cfb7e93c152171b59453e
umesh1321 <umesh.maurya.22@mountblue.tech>
2022-11-10 08:16:56 +0530

    stored the data from csv files

---
+++ Main.java
@@ -0,0 +1,102 @@
+import entity.Deliveries;
+import entity.Matches;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Matches> listOfMatches = new ArrayList<>();
+        ArrayList<Deliveries> listOfDeliveries = new ArrayList<>();
+
+        BufferedReader sc = new BufferedReader(new FileReader("/home/umesh/IdeaProjects/IPL/Data/matches.csv"));
+        String line="";
+        String splitBy=",";
+        while ((line=sc.readLine())!=null)  //returns a boolean value
+        {
+            String[] match=line.split(splitBy);
+            Matches matches = new Matches();
+            matches.setId(match[0]);
+            matches.setSeason(match[1]);
+            matches.setCity(match[2]);
+            matches.setDate(match[3]);
+            matches.setTeam1(match[4]);
+            matches.setTeam2(match[5]);
+            matches.setToss_winner(match[6]);
+            matches.setToss_decision(match[7]);
+            matches.setResult(match[8]);
+            matches.setDl_applied(match[9]);
+            matches.setWinner(match[10]);
+            matches.setWin_by_runs(match[11]);
+            matches.setWin_by_wickets(match[12]);
+            matches.setPlayer_of_the_match(match[13]);
+            matches.setVenue(match[14]);
+
+            if(match.length>15) {
+                matches.setUmpire1(match[15]);
+            }
+
+            if(match.length>16) {
+                matches.setUmpire2(match[16]);
+            }
+
+            if(match.length>17) {
+                matches.setUmpire3(match[17]);
+            }
+            listOfMatches.add(matches);
+
+        }
+        sc.close();
+
+
+
+        sc = new BufferedReader(new FileReader("/home/umesh/IdeaProjects/IPL/Data/deliveries.csv"));
+        String line1="";
+        String splitBy1=",";
+        while ((line1=sc.readLine())!=null)  //returns a boolean value
+        {
+            String[] delivery = line1.split(splitBy1);
+            Deliveries deliveries = new Deliveries();
+            deliveries.setMatch_id(delivery[0]);
+            deliveries.setInnings(delivery[1]);
+            deliveries.setBatting_team(delivery[2]);
+            deliveries.setBowling_team(delivery[3]);
+            deliveries.setOver(delivery[4]);
+            deliveries.setBall(delivery[5]);
+            deliveries.setBatsman(delivery[6]);
+            deliveries.setNon_striker(delivery[7]);
+            deliveries.setBowler(delivery[8]);
+            deliveries.setIs_super_over(delivery[9]);
+            deliveries.setWide_runs(delivery[10]);
+            deliveries.setBye_runs(delivery[11]);
+            deliveries.setLegbye_runs(delivery[12]);
+            deliveries.setNoball_runs(delivery[13]);
+            deliveries.setPenalty_runs(delivery[14]);
+            deliveries.setBatsman(delivery[15]);
+            deliveries.setExtra_runs(delivery[16]);
+            deliveries.setTotal_runs(delivery[17]);
+
+            if(delivery.length>18) {
+                deliveries.setPlayer_dismissed(delivery[18]);
+            }
+
+            if(delivery.length>19) {
+                deliveries.setDismissal_kind(delivery[19]);
+            }
+
+            if(delivery.length>20) {
+                deliveries.setFielder(delivery[20]);
+            }
+
+            listOfDeliveries.add(deliveries);
+        }
+        sc.close();
+
+
+
+
+    }
+}
---
+++ entity/Deliveries.java
@@ -0,0 +1,193 @@
+package entity;
+
+public class Deliveries {
+    String match_id;
+    String innings;
+    String batting_team;
+    String bowling_team;
+    String over;
+    String ball;
+    String batsman;
+    String non_striker;
+    String bowler;
+    String is_super_over;
+    String wide_runs;
+    String bye_runs;
+    String legbye_runs;
+    String noball_runs;
+    String penalty_runs;
+    String batsman_runs;
+    String extra_runs;
+    String total_runs;
+    String player_dismissed;
+    String dismissal_kind;
+    String fielder;
+
+    public String getMatch_id() {
+        return match_id;
+    }
+
+    public void setMatch_id(String match_id) {
+        this.match_id = match_id;
+    }
+
+    public String getInnings() {
+        return innings;
+    }
+
+    public void setInnings(String innings) {
+        this.innings = innings;
+    }
+
+    public String getBatting_team() {
+        return batting_team;
+    }
+
+    public void setBatting_team(String batting_team) {
+        this.batting_team = batting_team;
+    }
+
+    public String getBowling_team() {
+        return bowling_team;
+    }
+
+    public void setBowling_team(String bowling_team) {
+        this.bowling_team = bowling_team;
+    }
+
+    public String getOver() {
+        return over;
+    }
+
+    public void setOver(String over) {
+        this.over = over;
+    }
+
+    public String getBall() {
+        return ball;
+    }
+
+    public void setBall(String ball) {
+        this.ball = ball;
+    }
+
+    public String getBatsman() {
+        return batsman;
+    }
+
+    public void setBatsman(String batsman) {
+        this.batsman = batsman;
+    }
+
+    public String getNon_striker() {
+        return non_striker;
+    }
+
+    public void setNon_striker(String non_striker) {
+        this.non_striker = non_striker;
+    }
+
+    public String getBowler() {
+        return bowler;
+    }
+
+    public void setBowler(String bowler) {
+        this.bowler = bowler;
+    }
+
+    public String getIs_super_over() {
+        return is_super_over;
+    }
+
+    public void setIs_super_over(String is_super_over) {
+        this.is_super_over = is_super_over;
+    }
+
+    public String getWide_runs() {
+        return wide_runs;
+    }
+
+    public void setWide_runs(String wide_runs) {
+        this.wide_runs = wide_runs;
+    }
+
+    public String getBye_runs() {
+        return bye_runs;
+    }
+
+    public void setBye_runs(String bye_runs) {
+        this.bye_runs = bye_runs;
+    }
+
+    public String getLegbye_runs() {
+        return legbye_runs;
+    }
+
+    public void setLegbye_runs(String legbye_runs) {
+        this.legbye_runs = legbye_runs;
+    }
+
+    public String getNoball_runs() {
+        return noball_runs;
+    }
+
+    public void setNoball_runs(String noball_runs) {
+        this.noball_runs = noball_runs;
+    }
+
+    public String getPenalty_runs() {
+        return penalty_runs;
+    }
+
+    public void setPenalty_runs(String penalty_runs) {
+        this.penalty_runs = penalty_runs;
+    }
+
+    public String getBatsman_runs() {
+        return batsman_runs;
+    }
+
+    public void setBatsman_runs(String batsman_runs) {
+        this.batsman_runs = batsman_runs;
+    }
+
+    public String getExtra_runs() {
+        return extra_runs;
+    }
+
+    public void setExtra_runs(String extra_runs) {
+        this.extra_runs = extra_runs;
+    }
+
+    public String getTotal_runs() {
+        return total_runs;
+    }
+
+    public void setTotal_runs(String total_runs) {
+        this.total_runs = total_runs;
+    }
+
+    public String getPlayer_dismissed() {
+        return player_dismissed;
+    }
+
+    public void setPlayer_dismissed(String player_dismissed) {
+        this.player_dismissed = player_dismissed;
+    }
+
+    public String getDismissal_kind() {
+        return dismissal_kind;
+    }
+
+    public void setDismissal_kind(String dismissal_kind) {
+        this.dismissal_kind = dismissal_kind;
+    }
+
+    public String getFielder() {
+        return fielder;
+    }
+
+    public void setFielder(String fielder) {
+        this.fielder = fielder;
+    }
+}
